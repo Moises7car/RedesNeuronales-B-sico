3fa942d8eac5b5a150236ce10755e797c9338f7e
Moises SC <mseguracarrillo7@gmail.com>
2025-09-04 00:10:32 -0600

    Entrenamiento con Adam

--- train.py
+++ train.py
@@ -7,9 +7,9 @@ def main():
 
     # Definir la red: 784 neuronas de entrada, 30 ocultas, 10 de salida
     net = network.Network([784, 30, 10])
-
-    # Entrenar con SGD
-    net.SGD(training_data, epochs=10, mini_batch_size=10, eta=0.5, test_data=test_data)
+    
+   #Entrenar con SGD
+    net.SGD(training_data, epochs=10, mini_batch_size=32, eta=0.001, test_data=test_data)
 
 if __name__ == "__main__":
     main()
